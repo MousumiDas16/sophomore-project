572bbcbc20d8e9a86135dd2e3321b0e90974a28e
Mousumi Das <mousumidas@oakland.edu>
2020-10-20 03:38:38 -0400

    Integration and fixing the menus

--- src/sample/Player.java
+++ src/sample/Player.java
@@ -4,7 +4,8 @@ package sample;
 
 public class Player extends Entity {
 
-    //Variable Initialization
+
+
     private int Health;
     private double Money;
     private int Strength;
@@ -12,12 +13,41 @@ public class Player extends Entity {
     private int Speed;
     private int Charisma;
     private String Class;
+    private int Potions;
+    private int Statboost;
+    private int Clothes;
+    private int Food;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private  String description;
+
+
+
+    public String getPlayerClass() {
+        return Class;
+    }
+
 
     public Player(String name) {
         this.Name = name;
     }
 
-    // Get and Set Elements
+
+    public String getPlayerName() {
+        return Name;
+    }
+
+
+    public void setPlayerName(String name) {
+        this.Name = name;
+    }
 
 
     public double getMoney() {
@@ -68,6 +98,20 @@ public class Player extends Entity {
         Charisma = charisma;
     }
 
+    public void AddPotion(){
+        Potions++;
+
+    }
+    public void addFood(){
+        Food++;
+    }
+    public void addStatboost(){
+        Statboost++;
+    }
+    public void addClothes(){
+        Clothes++;
+    }
+
     public void setClass(String clss) {
         switch (clss.toLowerCase()) {
             case "wizard":
@@ -78,6 +122,12 @@ public class Player extends Entity {
                 Armor = 5;//BASELINE 10
                 Speed = 10;//BASELINE 10
                 Charisma = 5;//BASELINE 10
+                Potions=0;
+                description=" Always an intelligent youth, you would read and reread all the books your mother could\n " +
+                        "afford. After reading you studied your hometown and observed the other residents as well as\n" +
+                        " listening to any traveler who came by the Brewery. You could not be satisfied with the mundane world of Donkey’s\n " +
+                        "Town and traveled south to the Grand University of the Mage Guild. There you learned devastating spells but were\n " +
+                        "expelled for your lack of intrigue in the academics and obsession with powerful destruction\n magic. You returned home, an outcast…";
 
                 System.out.println("The players class is " + this.Class);
                 break;
@@ -91,6 +141,12 @@ public class Player extends Entity {
                 Armor = 15;
                 Speed = 5;
                 Charisma = 10;
+                Potions=0;
+                description="Always a strong and energetic child, you spent your youth training your physical prowess\n " +
+                        "and sharpening your skills with a sword, until you became a squire. After an upbringing as a \n" +
+                        "squire where you accompanied your mentor, Sir Reginald Fellings, you traveled the land seeking\n" +
+                        " renown. However, you could not build your reputation and you returned to your hometown, a failure...";
+
                 System.out.println("The players class is " + this.Class);
                 break;
 
@@ -102,6 +158,13 @@ public class Player extends Entity {
                 Armor = 10;
                 Speed = 10;
                 Charisma = 15;
+                Potions=0;
+                description="Growing up an orphan in the slums of Donkey’s Town, you became an expert pickpocket\n " +
+                        "and thief. Being self-taught your confidence grew until one day you stole from the wrong man. \n" +
+                        " Edward was another thief but far more experienced, He took you under his wing and you spent \n" +
+                        " years travelling from town to town swindling and thieving along the way. " +
+                        "Edward instructed \n you in the ways of the sword and the art of sweet talking. After Edward was " +
+                        "killed in  \n a job gone wrong you travelled home to lick your wounds and find leads for a new job…";
                 System.out.println("The players class is " + this.Class);
                 break;
 
@@ -113,6 +176,12 @@ public class Player extends Entity {
                 Armor = 5;
                 Speed = 15;
                 Charisma = 10;
+                Potions=0;
+                description="ou were born an unremarkable child of unremarkable parents. " +
+                        "All your life you toiled\n away at your family's farm, learning the trade from your mother " +
+                        "and father. You were faster and braver than most of your peers.\n After the passing of your " +
+                        "parents you swore you would not die a lowly farmer but would leave Donkey’s Town\n" +
+                        " and seek a grand adventure…";
                 System.out.println("The players class is " + this.Class);
                 break;
 
@@ -126,5 +195,8 @@ public class Player extends Entity {
     }
 
 
+    public int getPotions() {
+        return  this.Potions;
+    }
 }
 
