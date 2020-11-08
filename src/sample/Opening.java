e1c5cf6914f47a0e90e299220e24a1c8e1164118
Mousumi Das <mousumidas@oakland.edu>
2020-11-08 02:47:04 -0500

    Improved UI orientation and game loading..

--- src/sample/Opening.java
+++ src/sample/Opening.java
@@ -14,15 +14,20 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
 /**
- * @author Tommy
+ * @author Tommy, Mousumi
  */
 public class Opening extends Application {
     static int next = 0;
@@ -47,35 +52,39 @@ public class Opening extends Application {
 
 
         ArrayList<String> words = new ArrayList<>();
-        words.add("In the Elderbrew tavern, \na mysterious traveling woman \ninforms a young villager about an ancient legend….");
-        words.add("In a cave atop Dragon’s Claw, an old \nand malformed mountain formation,\nthere rests a blade of unparalleled power.\n"+"The one who finds such a weapon will be\nrenowned across the land and will\n be made KING!!!\nThe sword is the rightful relic of the Old King and\nhis reincarnates…");
+        words.add("In the Elderbrew tavern, \na mysterious traveling woman \ninforms a young villager about \n an ancient legend….");
+        words.add("In a cave atop Dragon’s Claw, an old \nand malformed mountain formation,\nthere rests a blade of unparalleled power.\n"+
+                "The one who finds such a weapon will be\nrenowned across the land and will\n be made KING!!!\n" +
+                "\nThe sword is the rightful relic of the Old \n King and his reincarnates…");
         Text Line1 = new Text();
         root.getChildren().add(Line1);
         Line1.setText(words.get(next));
         Line1.setStroke(Color.WHEAT);
-        Button submit = new Button("NEXT");
-        submit.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                next++;
-                if(next <= words.size() -1){
-                    Line1.setText(words.get(next));
-                }else{
-
-                    Scene s1 = Tavern.main(x, hero);// next town pic please
-                    x.setScene(s1);
-                }
+        Line1.setFont( Font.font("Verdana", FontWeight.BOLD, 18));
 
 
+        Image img = new Image("sample/Art/Background/arrow.png", 100, 100, true, true);
+        ImageView nxtView = new ImageView(img);
+        nxtView.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        System.out.println("Next pressed ");
+                        next++;
+                        if(next <= words.size() -1){
+                            Line1.setText(words.get(next));
+                        }else{
+                            Tavern scene=new Tavern();
+                            Scene s1 = scene.getScene(x, hero);// next town pic please
+                            x.setScene(s1);
+                        }
+                        event.consume();
+                    }
 
-            }
+                });
 
-            ;
-
-        });
-        submit.setTranslateX(Line1.getX()+ 200);
-        root.getChildren().add(submit);
+        nxtView.setTranslateX(Line1.getX()+120);
+        nxtView.setTranslateY(Line1.getY()+ 160);
+        root.getChildren().add(nxtView);
 
         Scene S2 = new Scene(root, 750, 500);
 
