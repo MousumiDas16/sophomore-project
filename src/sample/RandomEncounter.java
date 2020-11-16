775bfd56af184ccb06aa69c7631fe5c46f13c17d
JMOakland <71767778+JMOakland@users.noreply.github.com>
2020-11-16 16:50:42 -0500

    u

--- src/sample/RandomEncounter.java
+++ src/sample/RandomEncounter.java
@@ -384,7 +384,7 @@ public class RandomEncounter extends Application {
                                 int earned = rand.nextInt(200);
                                 gold = earned + gold;
                                 hero.setMoney(gold);
-                                popup4.setText("you took gold " + earned + " from the " + enemy.getType());
+                                popup4.setText("you took " + earned + " gold from the " + enemy.getType());
                                 statController.updateStats();
 
 
